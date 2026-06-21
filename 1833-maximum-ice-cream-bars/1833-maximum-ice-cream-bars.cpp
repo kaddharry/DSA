150ca30ca8e8104ff27cac3da765afa758277029
Hardik Kadd <161879911+kaddharry@users.noreply.github.com>
2026-06-21 23:55:44 +0530

    Time: 38 ms (33.97%), Space: 80.3 MB (37.51%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -3,6 +3,7 @@ public:
     int maxIceCream(vector<int>& costs, int coins) {
         sort(costs.begin(), costs.end());
         int count = 0;
+        if(costs[0] > coins) return 0;
         for (int x : costs) {
             if (x > coins)
                 break;
