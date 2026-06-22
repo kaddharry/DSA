3c2b6f5e3697e4d6f11bf09b67a6739830720c6e
Hardik Kadd <161879911+kaddharry@users.noreply.github.com>
2026-06-22 23:29:19 +0530

    Time: 2 ms (19.9%), Space: 8.7 MB (95.38%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        unordered_map <char, int> mp;
-        for(char c: text){
-            mp[c]++;
-        }
-        mp['l'] /= 2;
-        mp['o'] /= 2;
-        int count = INT_MAX;
-        for(char k: string("balon")){
-            if(mp[k] == 0) return 0;
-            int val = mp[k];
-            count = min(val, count);
-        }
-        return count;
+        int freq[26] = {0};
+
+        for(char c : text)
+            freq[c - 'a']++;
+
+        return min({
+            freq['b' - 'a'],
+            freq['a' - 'a'],
+            freq['l' - 'a'] / 2,
+            freq['o' - 'a'] / 2,
+            freq['n' - 'a']
+        });
     }
 };
+// class Solution {
+// public:
+//     int maxNumberOfBalloons(string text) {
+//         unordered_map <char, int> mp;
+//         for(char c: text){
+//             mp[c]++;
+//         }
+//         mp['l'] /= 2;
+//         mp['o'] /= 2;
+//         int count = INT_MAX;
+//         for(char k: string("balon")){
+//             if(mp[k] == 0) return 0;
+//             int val = mp[k];
+//             count = min(val, count);
+//         }
+//         return count;
+//     }
+// };
