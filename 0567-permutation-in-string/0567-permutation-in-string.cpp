43c6f2298a6d2a81bb7d40ac3e2dd1a6a371344e
Hardik Kadd <161879911+kaddharry@users.noreply.github.com>
2026-07-06 03:06:29 +0530

    Time: 3 ms (65.61%), Space: 10 MB (70.98%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,14 +1,22 @@
 class Solution {
-public:
+   public:
     bool checkInclusion(string s1, string s2) {
-        if(s1.size() > s2.size()) return false;
-        int k = s1.size(); //window size
-        sort(s1.begin(),s1.end());
-        string comp = s1;
-        for(int i = 0; i < s2.size() - k + 1; i++){
-            string temp = s2.substr(i, k);
-            sort(temp.begin(), temp.end());
-            if(comp == temp) return true;
+        if (s1.size() > s2.size()) return false;
+        int k = s1.size();  // window size
+
+        vector<int> fq1(26,0);
+        vector<int> fq2(26,0);
+
+        for (char x : s1) fq1[x - 'a']++;
+        int l = 0, r = 0;
+        while (r < s2.size()) {
+            fq2[s2[r] - 'a']++;
+            if (r - l + 1 == k) {
+                if (fq1 == fq2) return true;
+                fq2[s2[l] - 'a']--;
+                l++;
+            }
+            r++;
         }
         return false;
     }
