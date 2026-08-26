912f546a013fc125ffd15909a99a23b0c6581121
Hardik Kadd <161879911+kaddharry@users.noreply.github.com>
2026-08-26 21:26:20 +0530

    Time: 0 ms (100%), Space: 8.5 MB (39.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,13 @@
 class Solution {
-    unordered_map <int,int> memo;
 public:
     int climbStairs(int n) {
-        if(n == 1) return 1;
-        if(n == 2) return 2;
+        vector<int> dp(n+1,-1);
+        dp[0] = 1;
+        dp[1] = 1;
 
-        if(memo.count(n)) return memo[n];
-        memo[n] = climbStairs(n-1) + climbStairs(n-2);
-        return memo[n];
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        } 
+        return dp[n];
     }
 };
