bfce1974432249be816cd670a004ccf63b516884
Hardik Kadd <161879911+kaddharry@users.noreply.github.com>
2026-09-01 19:17:11 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.61%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,19 +3,14 @@ public:
     void moveZeroes(vector<int>& nums) {
         if(nums.size() <= 1) return;
         int p = 0;
-        int counter = 0;
         for(int i = 0; i < nums.size(); i++){
             if(nums[i] != 0){
                 nums[p] = nums[i];
                 p++;
             }
-            else counter++;
         }
-        p = nums.size()-1;
-        while(counter){
+        for(int i = p; p < nums.size(); p++){
             nums[p] = 0;
-            counter--;
-            p--;
         }
     }
 };
