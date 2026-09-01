1ee9b353708b9a326909cff0f929ff0b503b1ed4
Hardik Kadd <161879911+kaddharry@users.noreply.github.com>
2026-09-01 16:12:40 +0530

    Time: 0 ms (100%), Space: 9.6 MB (25.25%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int secondHighest(string s) {
-        vector<int> upd;
-        for(char x: s){
-            if(isdigit(x)){
-                upd.push_back(x - '0');
-            }
-        }
-        if(upd.empty()) return -1; // no digit in string
-
         int first = INT_MIN, sec = INT_MIN;
-        for(int num: upd){
-            if(first < num){
-                sec = first;
-                first = num;
-            }
-            else if(first > num && num > sec){
-                sec = num;
+
+        for (char x : s) {
+            if (isdigit(x)) {
+                int num = x - '0'; // Convert on the fly
+                
+                if (num > first) {
+                    sec = first;
+                    first = num;
+                } else if (num < first && num > sec) {
+                    sec = num;
+                }
             }
         }
         
