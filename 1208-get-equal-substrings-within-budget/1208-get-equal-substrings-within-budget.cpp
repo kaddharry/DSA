53809611f3815644013a1544864fc1375c85a893
Hardik Kadd <161879911+kaddharry@users.noreply.github.com>
2026-09-03 23:28:52 +0530

    Time: 1 ms (45.43%), Space: 10.2 MB (56.58%) - LeetHub

---
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        int len = 0;
+        int cost = 0;
+        int l = 0;
+        for(int i = 0; i < s.size(); ++i){
+            char a = s[i];
+            char b = t[i];
+            cost += abs(a - b);
+            while(cost > maxCost && l <= i){
+                char x = s[l];
+                char y = t[l];
+                cost -= abs(x - y);
+                l++;
+            }
+
+            if(cost <= maxCost) len = max(len, i - l +1);
+        }
+        return len;
+    }
+};
