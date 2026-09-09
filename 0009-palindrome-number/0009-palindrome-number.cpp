ef0b7f2ef9cd2b6f416eb64e099e13dbcd73e842
Hardik Kadd <161879911+kaddharry@users.noreply.github.com>
2026-09-10 00:46:15 +0530

    Time: 0 ms (100%), Space: 10.9 MB (7.06%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0) return false;
-        stack<int> st;
-        int temp = x;
-        int n = 0;
-        while(temp){
-            st.push(temp%10);
-            temp /= 10;
-            n++;
-        }
-        temp = x;
-        n /= 2;
-        while(n){
-            int y = st.top();
-            if(y != temp%10) return false;
-            st.pop();
-            temp /= 10;
-            n--;
+        if (x < 0)
+            return false;
+        string s = to_string(x);
+        for (int i = 0; i < s.size() / 2; i++) {
+            if (s[i] != s[s.size() - i - 1])
+                return false;
         }
         return true;
     }
